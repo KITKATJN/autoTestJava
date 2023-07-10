0e2367c4e712dba2ac1ddd34a197a523ab7c6048
KITKATJN <lvutinilyha@gmail.com>
2023-07-10 22:49:53 +0200

    add: test class and tests

---
+++ src/main/StringReverse.java
@@ -0,0 +1,15 @@
+package main;
+
+public class StringReverse {
+
+    /**
+     *
+     * @param stringToReverse string for reverse
+     * @return reversed string
+     */
+    public static String stringReverse(String stringToReverse) {
+        // insert your code here
+        // you need to convert string to array of chars and work with it
+        return stringToReverse;
+    }
+}
---
+++ src/test/TestStringReverse.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static main.StringReverse.stringReverse;
+
+public class TestStringReverse {
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(new StringBuilder("aaa").reverse().toString(), stringReverse("aaa"));
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(new StringBuilder("\088").reverse().toString(), stringReverse("\088"));
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals(new StringBuilder("a56aa\0TTT").reverse().toString(), stringReverse("a56aa\0TTT"));
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertEquals(new StringBuilder("aaa438234f9821@!#13x213x21x4TTT").reverse().toString(), stringReverse("aaa438234f9821@!#13x213x21x4TTT"));
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertEquals(new StringBuilder("\0\0\0\0\0\0").reverse().toString(), stringReverse("\0\0\0\0\0\0"));
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertNull(stringReverse(null));
+    }
+
+}
